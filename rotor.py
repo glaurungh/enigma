243935088ff0102a7bac43b85a9d7d7cacabe9d8
Vyacheslav V. Tomilin <glaurungh@gmail.com>
2021-10-20 19:40:38 +0300

    Initial commit: rotor function

---
+++ rotor.py
@@ -0,0 +1,18 @@
+def rotor(symbol, n, reverse=False):
+    ROTORS = {0: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+          1: 'EKMFLGDQVZNTOWYHXUSPAIBRCJ',
+          2: 'AJDKSIRUXBLHWTMCQGZNPYFVOE',
+          3: 'BDFHJLCPRTXVZNYEIWGAKMUSQO',
+          4: 'ESOVPZJAYQUIRHXLNFTGKDCMWB',
+          5: 'VZBRGITYUPSDNHLXAWMJQOFECK',
+          6: 'JPGVOUMFYQBENHZRDKASXLICTW',
+          7: 'NZJHGRCXMYSWBOUFAIVLPEKQDT', 
+          8: 'FKQHTLXOCBJSPDZRAMEWNIUYGV',
+          'beta': 'LEYJVCNIXWPBQMDRTAKZGFUHOS',
+          'gamma': 'FSOKANUERHMBTIYCWLQPZXVGJD'
+          }
+    if reverse:
+        idx = ROTORS[n].index(symbol)
+        return ROTORS[0][idx]
+    return ROTORS[n][ROTORS[0].index(symbol)]
+
