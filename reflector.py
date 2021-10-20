fbb01ba3363812601f0012d4cc77ac4066e6cbe1
Vyacheslav V. Tomilin <glaurungh@gmail.com>
2021-10-20 19:44:11 +0300

    Added reflector function

---
+++ reflector.py
@@ -0,0 +1,8 @@
+def reflector(symbol, n):
+    REFLECTORS = {0: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+                  1: 'YRUHQSLDPXNGOKMIEBFZCWVJAT',
+                  2: 'FVPJIAOYEDRZXWGCTKUQSBNMHL',
+                  3: 'ENKQAUYWJICOPBLMDXZVFTHRGS',
+                  4: 'RDOBJNTKVEHMLFCWZAXGYIPSUQ',
+                  }
+    return "".join([REFLECTORS[n][REFLECTORS[0].index(s)] for s in symbol])
